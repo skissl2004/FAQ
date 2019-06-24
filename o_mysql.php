a6797495c54db00323def558b3efa9adf7b71f2b
skissl2004 <44352393+skissl2004@users.noreply.github.com>
2019-06-24 16:40:20 +0800

    0624_o_mysql_3306_2

--- o_mysql.php
+++ o_mysql.php
@@ -1,21 +1,17 @@
 <?php
 // $con=mysqli_init(); 
 // mysqli_real_connect($con, "laraveldb.mysql.database.azure.com", "laraveldb@laraveldb", "Toppath@2019", "mysql", 3306);
-$DBNAME = "mysql";
-$DBUSER = "laraveldb@laraveldb";
-$DBPASSWD = "Toppath@2019";
-$DBHOST = "laraveldb.mysql.database.azure.com";
-$DBPORT = 3306;
-$conn = mysqli_connect( $DBHOST, $DBUSER, $DBPASSWD,$DBPORT);
-if (empty($conn)){
-  print mysqli_error($conn);
-  die ("無法連結資料庫");
-  exit;
+$host = 'mydemoserver.mysql.database.azure.com';
+$username = 'myadmin@mydemoserver';
+$password = 'Toppath@2019';
+$db_name = 'mysql';
+
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-if( !mysqli_select_db($conn, $DBNAME)) {
-  die ("無法選擇資料庫");
-}
-mysqli_query( $conn, "SET NAMES 'utf8'");// 設定連線編碼
 
 
 
